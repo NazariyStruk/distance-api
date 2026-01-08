7cf4d2473a353b13dbe29800e852a764686e6537
nazar.struk <struk.nazariy@gmail.com>
2026-01-08 19:30:22 +0200

    testing

--- src/main/java/com/fuchs/invoicesParser/InvoiceController.java
+++ src/main/java/com/fuchs/invoicesParser/InvoiceController.java
@@ -1,25 +1,25 @@
-package com.fuchs.invoicesParser;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/api/invoices")
-public class InvoiceController {
-
-    private final InvoiceParserService parserService;
-
-    public InvoiceController(InvoiceParserService parserService) {
-        this.parserService = parserService;
-    }
-
-    @PostMapping("/parse")
-    public ResponseEntity<String> parseInvoiceItem(@RequestBody InvoiceRequestDto request) {
-        try {
-            parserService.parseAndSave(request);
-            return ResponseEntity.ok("Item parsed and saved successfully");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error parsing item: " + e.getMessage());
-        }
-    }
-}
+//package com.fuchs.invoicesParser;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api/invoices")
+//public class InvoiceController {
+//
+//    private final InvoiceParserService parserService;
+//
+//    public InvoiceController(InvoiceParserService parserService) {
+//        this.parserService = parserService;
+//    }
+//
+////    @PostMapping("/parse")
+////    public ResponseEntity<String> parseInvoiceItem(@RequestBody InvoiceRequestDto request) {
+////        try {
+////            parserService.parseAndSave(request);
+////            return ResponseEntity.ok("Item parsed and saved successfully");
+////        } catch (Exception e) {
+////            return ResponseEntity.badRequest().body("Error parsing item: " + e.getMessage());
+////        }
+////    }
+//}
